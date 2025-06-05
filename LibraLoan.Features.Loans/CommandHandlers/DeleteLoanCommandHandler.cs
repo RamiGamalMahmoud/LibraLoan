faffbfcfc336562d4814103b806bb02b4bcf5469
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-06-06 01:05:05 +0300

    Create DeleteLoanCommandHandler.cs

---
+++ LibraLoan.Features.Loans/CommandHandlers/DeleteLoanCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using LibraLoan.Core.Abstraction.Repositories;
+using LibraLoan.Core.Commands;
+using LibraLoan.Core.Models;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LibraLoan.Features.Loans.CommandHandlers
+{
+    internal class DeleteLoanCommandHandler(ILoansRepository repository) : IRequestHandler<Core.Commands.Common.DeleteCommand<Loan>, bool>
+    {
+        public async Task<bool> Handle(Common.DeleteCommand<Loan> request, CancellationToken cancellationToken)
+        {
+            return await repository.DeleteAsync(request.Model);
+        }
+    }
+}
